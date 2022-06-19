ee0d5564f94060ce15f2b8250c13058c0dccbec6
Angie1402 <107798306+Angie1402@users.noreply.github.com>
2022-06-19 12:39:39 -0500

    Add files via upload

---
+++ Principal/naves.Designer.vb
@@ -0,0 +1,205 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class naves
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblId = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtTipo = New System.Windows.Forms.TextBox()
+        Me.txtCaracteristicas = New System.Windows.Forms.TextBox()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.dgvDatos = New System.Windows.Forms.DataGridView()
+        Me.btnNuevo = New System.Windows.Forms.Button()
+        Me.btnModificar = New System.Windows.Forms.Button()
+        Me.btnCancelar = New System.Windows.Forms.Button()
+        Me.btnEliminar = New System.Windows.Forms.Button()
+        Me.btnCerrar = New System.Windows.Forms.Button()
+        CType(Me.dgvDatos, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(188, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(114, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "REGISTRO DE NAVES"
+        '
+        'lblId
+        '
+        Me.lblId.AutoSize = True
+        Me.lblId.Location = New System.Drawing.Point(21, 32)
+        Me.lblId.Name = "lblId"
+        Me.lblId.Size = New System.Drawing.Size(18, 15)
+        Me.lblId.TabIndex = 1
+        Me.lblId.Text = "ID"
+        Me.lblId.Visible = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(21, 67)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(33, 15)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Tipo:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(21, 102)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(86, 15)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Caracteristicas:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(210, 67)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(112, 15)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Nombre de la Nave:"
+        '
+        'txtTipo
+        '
+        Me.txtTipo.Location = New System.Drawing.Point(60, 64)
+        Me.txtTipo.Name = "txtTipo"
+        Me.txtTipo.Size = New System.Drawing.Size(100, 23)
+        Me.txtTipo.TabIndex = 7
+        '
+        'txtCaracteristicas
+        '
+        Me.txtCaracteristicas.Location = New System.Drawing.Point(12, 120)
+        Me.txtCaracteristicas.Multiline = True
+        Me.txtCaracteristicas.Name = "txtCaracteristicas"
+        Me.txtCaracteristicas.Size = New System.Drawing.Size(491, 95)
+        Me.txtCaracteristicas.TabIndex = 8
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(328, 64)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(162, 23)
+        Me.txtNombre.TabIndex = 9
+        '
+        'dgvDatos
+        '
+        Me.dgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvDatos.Location = New System.Drawing.Point(12, 221)
+        Me.dgvDatos.Name = "dgvDatos"
+        Me.dgvDatos.RowTemplate.Height = 25
+        Me.dgvDatos.Size = New System.Drawing.Size(491, 150)
+        Me.dgvDatos.TabIndex = 10
+        '
+        'btnNuevo
+        '
+        Me.btnNuevo.Location = New System.Drawing.Point(12, 386)
+        Me.btnNuevo.Name = "btnNuevo"
+        Me.btnNuevo.Size = New System.Drawing.Size(75, 23)
+        Me.btnNuevo.TabIndex = 11
+        Me.btnNuevo.Text = "Nuevo"
+        Me.btnNuevo.UseVisualStyleBackColor = True
+        '
+        'btnModificar
+        '
+        Me.btnModificar.Location = New System.Drawing.Point(93, 386)
+        Me.btnModificar.Name = "btnModificar"
+        Me.btnModificar.Size = New System.Drawing.Size(75, 23)
+        Me.btnModificar.TabIndex = 12
+        Me.btnModificar.Text = "Modificar"
+        Me.btnModificar.UseVisualStyleBackColor = True
+        '
+        'btnCancelar
+        '
+        Me.btnCancelar.Location = New System.Drawing.Point(348, 386)
+        Me.btnCancelar.Name = "btnCancelar"
+        Me.btnCancelar.Size = New System.Drawing.Size(75, 23)
+        Me.btnCancelar.TabIndex = 13
+        Me.btnCancelar.Text = "Cancelar"
+        Me.btnCancelar.UseVisualStyleBackColor = True
+        '
+        'btnEliminar
+        '
+        Me.btnEliminar.Location = New System.Drawing.Point(174, 386)
+        Me.btnEliminar.Name = "btnEliminar"
+        Me.btnEliminar.Size = New System.Drawing.Size(75, 23)
+        Me.btnEliminar.TabIndex = 14
+        Me.btnEliminar.Text = "Eliminar"
+        Me.btnEliminar.UseVisualStyleBackColor = True
+        '
+        'btnCerrar
+        '
+        Me.btnCerrar.Location = New System.Drawing.Point(428, 386)
+        Me.btnCerrar.Name = "btnCerrar"
+        Me.btnCerrar.Size = New System.Drawing.Size(75, 23)
+        Me.btnCerrar.TabIndex = 15
+        Me.btnCerrar.Text = "Cerrar"
+        Me.btnCerrar.UseVisualStyleBackColor = True
+        '
+        'naves
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(515, 416)
+        Me.Controls.Add(Me.btnCerrar)
+        Me.Controls.Add(Me.btnEliminar)
+        Me.Controls.Add(Me.btnCancelar)
+        Me.Controls.Add(Me.btnModificar)
+        Me.Controls.Add(Me.btnNuevo)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.txtCaracteristicas)
+        Me.Controls.Add(Me.txtTipo)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblId)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.dgvDatos)
+        Me.Name = "naves"
+        Me.Text = "naves"
+        CType(Me.dgvDatos, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblId As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtTipo As TextBox
+    Friend WithEvents txtCaracteristicas As TextBox
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents dgvDatos As DataGridView
+    Friend WithEvents btnNuevo As Button
+    Friend WithEvents btnModificar As Button
+    Friend WithEvents btnCancelar As Button
+    Friend WithEvents btnEliminar As Button
+    Friend WithEvents btnCerrar As Button
+End Class
---
+++ Principal/naves.vb
@@ -0,0 +1,103 @@
+﻿Imports Datos
+Imports Entidad
+Imports System.Data
+Imports System.Data.SqlClient
+Public Class naves
+    Dim obd As New NavesDA
+    Dim obe As New NavesEN
+
+    Sub Limpiar()
+        txtTipo.Clear()
+        txtNombre.Clear()
+        txtCaracteristicas.Clear()
+        lblId.Text = 0
+    End Sub
+    Sub Listar_Naves()
+        Dim dt As DataTable = obd.Listar_Naves()
+        dgvDatos.DataSource = dt
+    End Sub
+    Private Sub naves_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Listar_Naves()
+
+    End Sub
+
+    Private Sub btnNuevo_Click(sender As Object, e As EventArgs) Handles btnNuevo.Click
+        If btnNuevo.Text = "Nuevo" Then
+            btnNuevo.Text = "Guardar"
+            txtCaracteristicas.Enabled = True
+        ElseIf btnNuevo.Text = "Guardar" Then
+
+            obe.tipo = txtTipo.Text
+            obe.nave = txtNombre.Text
+            obe.CaracteristicasNaves = txtCaracteristicas.Text
+
+
+
+            obd.Registrar_Naves(obe)
+
+
+            MsgBox("Registrado Correctamente")
+
+            Listar_Naves()
+
+            Limpiar()
+        End If
+    End Sub
+
+    Private Sub dgvDatos_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvDatos.CellContentClick
+
+    End Sub
+
+    Private Sub dgvDatos_CellDoubleClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvDatos.CellDoubleClick
+        Dim i As Integer
+        i = dgvDatos.CurrentRow.Index
+
+        txtCaracteristicas.Text = dgvDatos.Rows(i).Cells("3").Value
+        lblId.Text = dgvDatos.Rows(i).Cells("0").Value
+        txtTipo.Text = dgvDatos.Rows(i).Cells("1").Value
+        txtNombre.Text = dgvDatos.Rows(i).Cells("2").Value
+
+
+
+
+    End Sub
+
+    Private Sub btnModificar_Click(sender As Object, e As EventArgs) Handles btnModificar.Click
+        If btnModificar.Text = "Modificar" Then
+            btnModificar.Text = "Actualizar"
+            txtCaracteristicas.Enabled = True
+        ElseIf btnModificar.Text = "Actualizar" Then
+
+            obe.id_nave = Val(lblId.Text)
+            obe.tipo = txtTipo.Text
+            obe.nave = txtNombre.Text
+            obe.CaracteristicasNaves = txtCaracteristicas.Text
+
+            obd.Modificar_Naves(obe)
+
+
+            MsgBox("Modificado Correctamente")
+
+            Listar_Naves()
+            Limpiar()
+
+        End If
+    End Sub
+
+
+
+    Private Sub btnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
+        obe.id_nave = Val(lblId.Text)
+
+
+        obd.Eliminar_Naves(obe)
+
+
+        MsgBox("Nave Eliminada Correctamente")
+
+        Listar_Naves()
+
+        Limpiar()
+
+    End Sub
+End Class
